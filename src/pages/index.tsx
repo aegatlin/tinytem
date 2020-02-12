1efef32edfd998f29c625be569a5fe03cbe6703a
Austin Gatlin <austin@gatlin.io>
2020-02-11 21:38:23 -0500

    Add ability to add todo (with no custom text).

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
 import { client, Layout } from '../code/Layout'
 import { TodoUI } from '../code/Todo'
+import { useState, useEffect } from 'react'
 
 const TODOS = gql`
   {
@@ -13,9 +14,36 @@ const TODOS = gql`
   }
 `
 
+const CREATE_TODO = gql`
+  mutation {
+    createTodo(data: { title: "Eat Chocolate", completed: false }) {
+      title
+      completed
+    }
+  }
+`
+
 export default () => {
-  const { loading, error, data } = useQuery(TODOS, { client })
-  console.log(data)
+  const { loading, error, data, refetch } = useQuery(TODOS, { client })
+  const [createTodo, result] = useMutation(CREATE_TODO, { client })
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    if (data) {
+      setTodos(data.allTodos.data)
+    }
+  }, [data])
+
+  const addTodoHandler = () => {
+    createTodo()
+
+    if (result.data) {
+      let newTodo = result.data.createTodo
+      newTodo.title = newTodo.title + 'you been hacked'
+      setTodos(todos.concat([newTodo]))
+    }
+
+    setTimeout(() => refetch(), 3000)
+  }
 
   if (loading) return <Layout>Loading...</Layout>
   if (error) return <Layout>Error</Layout>
@@ -24,7 +52,12 @@ export default () => {
     <Layout>
       <h2>Todo List</h2>
       <div>
-        {data.allTodos.data.map((todo, i) => (
+        <label htmlFor="add-todo">Add Todo: </label>
+        <input type="text" name="add-todo" id="add-todo" />
+        <button onClick={addTodoHandler}>Submit</button>
+      </div>
+      <div>
+        {todos.map((todo, i) => (
           <TodoUI key={i} todo={todo} />
         ))}
       </div>
