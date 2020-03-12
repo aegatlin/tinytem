263e9b0e1cd76c19844df78308c9e9e2f3184fb9
Austin Gatlin <austin@gatlin.io>
2020-03-12 17:33:45 -0400

    Refactors.

---
+++ src/code/back/getApolloServerHandler.ts
@@ -0,0 +1,39 @@
+import { HttpLink } from 'apollo-link-http'
+import {
+  ApolloServer,
+  introspectSchema,
+  makeRemoteExecutableSchema
+} from 'apollo-server-micro'
+import { authenticate } from './authenticate'
+
+const link = new HttpLink({
+  uri: 'https://graphql.fauna.com/graphql',
+  fetch,
+  headers: {
+    Authorization: `Bearer ${process.env.FAUNADB_KEY}`
+  }
+})
+
+let apolloServerHandler: (req, res) => Promise<void>
+
+export const getApolloServerHandler = async () => {
+  if (apolloServerHandler) return apolloServerHandler
+
+  const schema = makeRemoteExecutableSchema({
+    schema: await introspectSchema(link),
+    link
+  })
+
+  const context = async ({ req, res }) => {
+    const [isAuthenticated, user] = await authenticate(req)
+    if (!isAuthenticated) {
+      res.statusCode = 401
+      res.end()
+    }
+    return { user }
+  }
+
+  const server = new ApolloServer({ schema, context })
+  apolloServerHandler = server.createHandler({ path: '/api/gql' })
+  return apolloServerHandler
+}
--- src/pages/api/gql.ts
+++ src/pages/api/gql.ts
@@ -1,56 +1,8 @@
-import { HttpLink } from 'apollo-link-http'
-import {
-  ApolloServer,
-  introspectSchema,
-  makeRemoteExecutableSchema
-} from 'apollo-server-micro'
-import { GraphQLSchema } from 'graphql'
-import fetch from 'isomorphic-unfetch'
-import { authenticate } from '../../code/back/authenticate'
-
-let schema: GraphQLSchema, server: ApolloServer
-
-const link = new HttpLink({
-  uri: 'https://graphql.fauna.com/graphql',
-  fetch,
-  headers: {
-    Authorization: `Bearer ${process.env.FAUNADB_KEY}`
-  }
-})
-
-const getSchema = async (): Promise<GraphQLSchema> => {
-  if (schema) return schema
-
-  schema = makeRemoteExecutableSchema({
-    schema: await introspectSchema(link),
-    link
-  })
-
-  return schema
-}
-
-const getServer = (schema: GraphQLSchema): ApolloServer => {
-  if (server) return server
-
-  server = new ApolloServer({
-    schema,
-    context: async ({ req, res }) => {
-      const [isAuthenticated, user] = await authenticate(req)
-
-      if (!isAuthenticated) {
-        res.statusCode = 401
-        res.end()
-      }
-
-      return { user }
-    }
-  })
-
-  return server
-}
+import { getApolloServerHandler } from '../../code/back/getApolloServerHandler'
 
 export const config = { api: { bodyParser: false } }
+
 export default async (req, res) => {
-  const server = getServer(await getSchema())
-  await server.createHandler({ path: '/api/gql' })(req, res)
+  const handler = await getApolloServerHandler()
+  await handler(req, res)
 }
